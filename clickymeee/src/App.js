613f20129abf4a545646c51f2b39701ab9169751
Mugencoder <cchavis3@gmail.com>
2019-04-14 19:42:31 -0400

    skeleton

--- clickymeee/src/App.js
+++ clickymeee/src/App.js
@@ -5,21 +5,43 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="root">
+        <div>
+          <nav class="navbar">
+            <ul>
+              <li class="brand">
+                <a href="/">Clicky Meee</a>
+              </li>
+              <li> WRONG GUESS, HOLMES</li>
+              <li>
+                "Score: "
+                "0"
+                " | Top Score: "
+                "1"
+              </li>
+            </ul>
+          </nav>
+            <header class="header">
+              <h1>Clicky Gaaamme!</h1>
+              <h2>Click on an image to earn points, but don't click any more than once!</h2>
+            </header>
+              <main class="container">
+                <div role="image" aria-label="click item" class="click item shake" style="background-image: url()"></div>
+                <div role="image" aria-label="click item" class="click item shake" style="background-image: url()"></div>
+                <div role="image" aria-label="click item" class="click item shake" style="background-image: url()"></div>
+                <div role="image" aria-label="click item" class="click item shake" style="background-image: url()"></div>
+                <div role="image" aria-label="click item" class="click item shake" style="background-image: url()"></div>
+                <div role="image" aria-label="click item" class="click item shake" style="background-image: url()"></div>
+                <div role="image" aria-label="click item" class="click item shake" style="background-image: url()"></div>
+                <div role="image" aria-label="click item" class="click item shake" style="background-image: url()"></div>
+                <div role="image" aria-label="click item" class="click item shake" style="background-image: url()"></div>
+              </main>
+                <footer class="footer">
+                  <div class="bottom">Clicky Gaaamme!
+                  <img alt="react" src="/"></img>
+                  </div>
+                </footer>
+        </div>
       </div>
     );
   }
