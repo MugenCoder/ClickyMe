0cc58041f42adb6fe59919f3d80f3e7b9158de0f
Mugencoder <cchavis3@gmail.com>
2019-04-15 20:07:30 -0400

    repush

--- clickymeee/src/App.js
+++ clickymeee/src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import FriendCard from "./components/FriendCard";
+// import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
-import guessies from "./guessies.json";
+// import guessies from "./guessies.json";
+import './App.css';
 
 class App extends Component {
+  handleClick= (e)
+  => {
+    setState({
+      clickCount: 0
+    })
+    }
+  }
   render() {
     return (
       <div id="root">
+      {/* create Wrapper Component 
+       */}
+        
         <div>
           <nav class="navbar">
             <ul>
@@ -26,24 +37,30 @@ class App extends Component {
               <h1>Clicky Gaaamme!</h1>
               <h2>Click on an image to earn points, but don't click any more than once!</h2>
             </header>
-              <main class="container">
-                <div role="image" aria-label="click item" class="click item shake" style="background-image: url()"></div>
-                <div role="image" aria-label="click item" class="click item shake" style="background-image: url()"></div>
-                <div role="image" aria-label="click item" class="click item shake" style="background-image: url()"></div>
-                <div role="image" aria-label="click item" class="click item shake" style="background-image: url()"></div>
-                <div role="image" aria-label="click item" class="click item shake" style="background-image: url()"></div>
-                <div role="image" aria-label="click item" class="click item shake" style="background-image: url()"></div>
-                <div role="image" aria-label="click item" class="click item shake" style="background-image: url()"></div>
-                <div role="image" aria-label="click item" class="click item shake" style="background-image: url()"></div>
-                <div role="image" aria-label="click item" class="click item shake" style="background-image: url()"></div>
+          
+        <Wrapper>
+              <main className="container">
+                <div role="image" onClick={this.handleClick} class="click item shake">1</div>
+
+                <div role="image" aria-label="click item" class="click item shake">2</div>
+
+                <div role="image" aria-label="click item" class="click item shake">3</div>
+
+                <div role="image" aria-label="click item" class="click item shake">4</div>
+
+                <div role="image" aria-label="click item" class="click item shake">5</div>
+
+                <div role="image" aria-label="click item" class="click item shake">6</div>
+
+                <div role="image" aria-label="click item" class="click item shake">7</div>
+
+                <div role="image" aria-label="click item" class="click item shake">8</div>
+
+                <div role="image" aria-label="click item" class="click item shake">9</div>
               </main>
-                <footer class="footer">
-                  <div class="bottom">Clicky Gaaamme!
-                  <img alt="react" src="/"></img>
-                  </div>
-                </footer>
-        </div>
-      </div>
+        </Wrapper>
+        </div> 
+      </div> // root
     );
   }
 }
--- clickymeee/src/components/Wrapper/index.jsx
+++ clickymeee/src/components/Wrapper/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 function Wrapper(props) {
-  console.log(props.children);
+  // console.log(props.children);
   return <div className="wrapper">{props.children}</div>;
 }
 
