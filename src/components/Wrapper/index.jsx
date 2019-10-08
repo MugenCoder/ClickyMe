0700ca8ed44e5481d7e7bfca9702d0d37e000c8b
Mugencoder <cchavis3@gmail.com>
2019-10-08 17:31:11 -0400

    clicky restart due to bugs

--- src/components/Wrapper/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./style.css";
-
-function Wrapper(props) {
-  // console.log(props.children);
-  return <div className="wrapper">{props.children}</div>;
-}
-
-export default Wrapper;
